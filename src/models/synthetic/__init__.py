155027c02bcfceeae7aa58701f98718287047947
MJSteynberg <120567081+MJSteynberg@users.noreply.github.com>
2025-10-29 10:19:07 +0100

    1018_29102025: Update

--- src/models/synthetic/__init__.py
+++
@@ -1,9 +0,0 @@
-# src/models/synthetic/__init__.py
-
-# This file makes your model classes importable
-
-# Import the base class so it can be accessed if needed
-from .base import SyntheticModel
-
-# Import your concrete model classes
-from .unet import UNet
